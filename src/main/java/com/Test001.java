2abc11cc3c303621f3a34f0e630012dba0f94684
yangshaohai <751941218@qq.com>
2019-07-19 09:30:57 +0800

    2019.7-19  JDBC修改

--- src/main/java/com/Test001.java
+++ src/main/java/com/Test001.java
@@ -8,12 +8,12 @@ import java.sql.Statement;
 
 public  class Test001 {
     public static void main(String[] args) {
-//        Connection conn = JdbcUtils.getConn();
-//        try {
-//            Statement statement = conn.createStatement();
-//            statement.execute("select * FROM users ");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        Connection conn = JdbcUtils.getConn();
+        try {
+            Statement statement = conn.createStatement();
+            statement.execute("select * FROM users ");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
